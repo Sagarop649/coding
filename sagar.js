89c30abdc751a39f362a3522f5841afa8f09a810
Sagarop649 <sagarchomu2@gmail.com>
2024-11-17 15:34:55 +0530

    dcndjnd,

---
+++ sagar.js
@@ -0,0 +1,46 @@
+// let arr12=[1,2,3]
+// let arr13=[0,2,3]
+// let intersaction=arr12.filter((value)=> arr13.includes(value));
+// console.log(intersaction);
+
+// let arr14=[1,2,3]
+// let arr15=[3,4,5,]
+// let newuniqueELearr =[...new Set([...arr14,...arr15])];
+// console.log(newuniqueELearr);
+
+// function subArrar(arr,size) {
+//     let chunks = [];
+//     for(let i=0;i<arr.length;i+= size){
+//         chunks.push(arr.slice(i,i+size));
+//     }
+//     return chunks;
+    
+// }
+
+// let res = subArrar([1,2,3,4,5,6,7,8,9,10],3);
+// console.log(res);
+
+// function sumOfNumber(number) {
+//     let sum =1;
+//     while (number >= 1) {
+//         sum*=number;
+//         number--;
+
+//     }
+//     return sum;
+
+    
+// }
+
+// let num =5;
+// console.log(sumOfNumber(num));
+
+
+
+// let arr1 =[]
+// arr1.push(10);
+// arr1.pop();
+// console.log(arr1);
+
+
+
